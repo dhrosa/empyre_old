4e1905407c0af20e7e76d010de79746d4af53843
Diony Rosa <dhrosa@gmail.com>
2011-06-14 05:03:25 -0400

    Added more to state machine.

--- common/game.py
+++ common/game.py
@@ -7,7 +7,6 @@ class State(object):
         OutOfSync,
         Lobby,
         ChoosingOrder,
-        OrderTie,
         InitialPlacement,
         CardExchange,
         Draft,
@@ -18,9 +17,70 @@ class State(object):
         GameOver,
     ) = range(1, 12)
 
-    def __init__(self, board, players):
+    def __init__(self, board):
         self.board = board
         self.players = []
-        self.substate = Lobby
-        self.order = []
+        self.livePlayers = []
         self.currentPlayer = None
+        self.firstPlayer = None
+        self.tiedPlayers = []
+        self.substate = Lobby
+        self.diceRolls = []
+        self.occupation = {}
+        self.remainingTroops = 0
+
+    def playerNames(self):
+        return [p.name for p in self.players]
+
+    def playerCount(self):
+        return len(self.players)
+
+    def tiedPlayerCount(self):
+        return len(self.tiedPlayers)
+
+    def livePlayerCount(self):
+        return len(self.livePlayers())
+
+    def territoryNames(self):
+        return [t.name for t in self.board.territories]
+
+    def isValidTerritory(self, t):
+        return t in self.territoryNames()
+
+class Action(object):
+    (
+        AddPlayer,
+        RemovePlayer,
+        StartGame,
+        RollDice,
+        ExchangeCards,
+        PlaceTroops,
+        Attack,
+        MoveTroops,
+        Fortify,
+        EndTurn,
+     ) = range(10)
+
+    validArgs = {
+        AddPlayer: (str, int),
+        RemovePlayer: (str),
+        StartGame: (),
+        RollDice: (),
+        ExchangeCards: (int, int, int),
+        PlaceTroops: (str, int),
+        Attack: (str, str, int),
+        MoveTroops: (str, str, int),
+        Fortify: (str, str, int),
+        EndTurn: (),
+    }
+
+    @staticmethod
+    def argMatch(action, args):
+        try:
+            valid = self.validArgs[action]
+            for i, a in enumerate(args):
+                if not type(a) == valid[i]:
+                    return False
+        except:
+            return False
+        return True
--- common/sm.py
+++ common/sm.py
@@ -1,42 +1,77 @@
-from game import *
+from game import State, Action
+
+def diceRoll(n):
+    return 7
 
 class SM(object):
-    (
-        AddPlayer,
-        RemovePlayer,
-        StartGame,
-        RollDice,
-        ExchangeCards,
-        PlaceTroops,
-        Attack,
-        MoveTroops,
-        Fortify,
-        EndTurn,
-     ) = range(10)
-
-    validArgs = {
-        AddPlayer: (str),
-        RemovePlayer: (str),
-        StartGame: (),
-        RollDice: (),
-        ExchangeCards: (int, int, int),
-        PlaceTroops: 2,
-        Attack: 2,
-        MoveTroops: 3,
-        Fortify: 3,
-        EndTurn: 0,
-    }
-
     def __init__(self):
         self.state = State()
 
-    def argMatch(self, action, args):
-        return len(args) == argCounts[action]
+    def nextPlayer(self, selection = None):
+        if not selection:
+            selection = self.state.livePlayers
+            i = (selection.index(self.currentPlayer) + 1) % len(selection)
+        return selection[i]
+
+    def draftCount(self, player):
+        return 1
 
     def next(self, action, args):
         s = self.state.substate:
-        if not argMatch(action, args):
-            return
+        if not Action.argMatch(action, args):
+            return False
         if s == State.Lobby:
-            if action == AddPlayer:
-                if 
+            if action == Action.AddPlayer:
+                if args[0] in self.state.playerNames():
+                    return False
+                self.state.players.append(Player(*args))
+                return True
+            elif action == Action.StartGame:
+                if len(self.state.players) < 2:
+                    return False
+                self.state.currentPlayer = self.state.players[0]
+                self.state.livePlayers = self.state.players
+                self.state.substate = State.ChoosingOrder
+                return True
+    
+        elif s == State.ChoosingOrder:
+            if action == Action.RollDice:
+                self.state.diceRolls.append(diceRoll(2))
+                if len(self.state.diceRolls) == self.state.tiedPlayerCount():
+                    highest = max(self.state.diceRolls)
+                    if self.state.diceRolls.count(highest) > 1:
+                        self.state.tiedPlayers = []
+                        for i, p in enumerate(self.state.tiedPlayers):
+                            if self.state.diceRolls[i] == highest:
+                                self.state.tiedPlayers.append(p)
+                        self.state.diceRolls = []
+                        return True
+                    else:
+                        index = self.state.diceRolls.index(highest)
+                        self.state.currentPlayer = self.state.tiedPlayers[i]
+                        self.state.firstPlayer = self.state.currentPlayer
+                        self.state.diceRolls = []
+                        self.state.substate = State.InitialPlacement
+                        return True
+                else:
+                    self.state.currentPlayer = self.nextPlayer(self.state.tiedPlayers)
+                    return True
+                
+        elif s == State.InitialPlacement:
+            if action == Action.PlaceTroops:
+                t = args[0]
+                if not t in self.state.territoryNames():
+                    return False
+                if t in self.state.occupation.keys():
+                    return False
+                self.state.occupation[t] = (self.state.currentPlayer, 1)
+                self.state.currentPlayer = self.nextPlayer()
+                if self.state.currentPlayer == self.state.firstPlayer():
+                    self.state.substate = Draft
+                return True
+
+        elif s == State.CardExchange:
+            pass
+        
+        elif s == State.Draft:
+            pass
