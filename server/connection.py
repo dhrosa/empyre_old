a9f7ee842c0eccffb340d2c2955d2cb2657a7435
Diony Rosa <dhrosa@gmail.com>
2011-06-16 14:20:12 -0400

    Fixed threading problem in server.

--- server/connection.py
+++ server/connection.py
@@ -6,27 +6,22 @@ class Connection(QThread):
 
     def __init__(self, id, parent = None):
         QThread.__init__(self, parent)
-        self.socket = QTcpSocket()
         self.id = id
-        self.finished.connect(QCoreApplication.quit)
+
+
+    def run(self):
+        self.socket = QTcpSocket()
         if not self.socket.setSocketDescriptor(self.id):
-            print "hi"
             self.socketError.emit(self.socket.error())
             return
         self.socket.setSocketOption(QTcpSocket.LowDelayOption, True)
-
-    def run(self):
         self.socket.waitForConnected(30000)
-        print self.socket.state()
         payload = "Hello world!"
         data = QByteArray()
         stream = QDataStream(data, QTcpSocket.WriteOnly)
         stream.writeInt32(len(payload))
         stream.writeRawData(payload)
         self.socket.write(data)
-        print self.socket.state()
         self.socket.waitForBytesWritten(30000)
-        print self.socket.state()
         self.socket.disconnectFromHost()
-        self.socket.waitForDisconnected(5000)
         return
--- server/server.py
+++ server/server.py
@@ -9,14 +9,20 @@ from PyQt4.QtCore import QCoreApplication
 class Server(QTcpServer):
     def __init__(self, parent = None):
        QTcpServer.__init__(self, parent)
+       self.connections = {}
 
     def incomingConnection(self, socketDescriptor):
-        self.c = Connection(socketDescriptor)
-        self.c.socketError.connect(self.socketErrorHandler)
-        self.c.start()
-
+        c = Connection(socketDescriptor)
+        c.socketError.connect(self.socketErrorHandler)
+        c.finished.connect(self.removeConnection)
+        self.connections[socketDescriptor] = c
+        c.start()
+        
     def socketErrorHandler(self, socketError):
         print socketError
+
+    def removeConnection(self):
+        del self.connections[self.sender().id]
 
 if __name__ == "__main__":
     app = QCoreApplication(sys.argv)
