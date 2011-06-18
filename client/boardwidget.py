717beebd7b829d890ff526b0274dde8106078c1c
Diony Rosa <dhrosa@gmail.com>
2011-06-18 05:23:31 -0400

    Implemented territory detection.

--- client/boardwidget.py
+++ client/boardwidget.py
@@ -5,7 +5,9 @@ class BoardWidget(QWidget):
     def __init__(self, game, parent = None):
         QWidget.__init__(self, parent)
         self.game = game
+        self.setMouseTracking(True)
         self.loadImages()
+        self.highlights = []
 
     def loadImages(self):
         self.game.board.image = QImage(self.game.board.image)
@@ -15,8 +17,7 @@ class BoardWidget(QWidget):
         for (i, name) in enumerate(names):
             progress.setValue(i)
             t = self.game.board.territories[name]
-            image = QImage()
-            t.image = image
+            t.image = QImage(t.image)
         self.resize(self.minimumSizeHint())
 
     def minimumSizeHint(self):
@@ -24,8 +25,21 @@ class BoardWidget(QWidget):
 
     def resizeEvent(self, event):
         self.scaledPixmap = QPixmap.fromImage(self.game.board.image).scaled(event.size(), Qt.KeepAspectRatio)
-        self.sx = self.game.board.image.width() / self.scaledPixmap.width()
-        self.sy = self.game.board.image.height() / self.scaledPixmap.height()
+        self.sx = float(self.game.board.image.width()) / self.scaledPixmap.width()
+        self.sy = float(self.game.board.image.height()) / self.scaledPixmap.height()
+
+    def territoryAt(self, x, y):
+        for t in self.game.board.territories.values():
+            if t.image.pixel(x, y):
+                return t
+
+    def mouseReleaseEvent(self, event):
+        pos = [event.pos().x(), event.pos().y()]
+        pos[0] *= self.sx
+        pos[1] *= self.sy
+        t = self.territoryAt(*pos)
+        if t:
+                print "Clicked on %s." % (t.name)
 
     def paintEvent(self, event):
         painter = QPainter(self)
