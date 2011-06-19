787b8072967008fa688f79112a624b84bf460718
Diony Rosa <dhrosa@gmail.com>
2011-06-18 21:08:51 -0400

    Improved board UI.

--- client/boardwidget.py
+++ client/boardwidget.py
@@ -1,13 +1,13 @@
 from PyQt4.QtCore import pyqtSignal, Qt
-from PyQt4.QtGui import QWidget, QImage, QProgressDialog, QPainter, QPixmap
+from PyQt4.QtGui import QWidget, QImage, QProgressDialog, QPainter, QPixmap, QColor
 
 class BoardWidget(QWidget):
     def __init__(self, game, parent = None):
         QWidget.__init__(self, parent)
         self.game = game
+        self.currentTerritory = None
         self.setMouseTracking(True)
         self.loadImages()
-        self.highlights = []
 
     def loadImages(self):
         self.game.board.image = QImage(self.game.board.image)
@@ -20,6 +20,25 @@ class BoardWidget(QWidget):
             t.image = QImage(t.image)
         self.resize(self.minimumSizeHint())
 
+    def recreateMasks(self):
+        self.masks = {}
+        self.selectionImages = {}
+        size = self.scaledPixmap.size()
+        rect = self.scaledPixmap.rect()
+        for t in self.game.board.territories.values():
+            mask = t.image.scaled(size)
+            self.masks[t] = mask
+            selection = QImage(size, QImage.Format_ARGB32_Premultiplied)
+            selection.fill(0)
+            painter = QPainter()
+            painter.begin(selection)
+            painter.setCompositionMode(QPainter.CompositionMode_Source)
+            painter.fillRect(rect, QColor(127, 127, 0, 127))
+            painter.setCompositionMode(QPainter.CompositionMode_DestinationIn)
+            painter.drawImage(0, 0, mask)
+            painter.end()
+            self.selectionImages[t] = selection
+
     def minimumSizeHint(self):
         return self.game.board.image.size()
 
@@ -27,21 +46,33 @@ class BoardWidget(QWidget):
         self.scaledPixmap = QPixmap.fromImage(self.game.board.image).scaled(event.size(), Qt.KeepAspectRatio)
         self.sx = float(self.game.board.image.width()) / self.scaledPixmap.width()
         self.sy = float(self.game.board.image.height()) / self.scaledPixmap.height()
+        self.recreateMasks()
 
     def territoryAt(self, x, y):
         for t in self.game.board.territories.values():
             if t.image.pixel(x, y):
                 return t
 
+    def mouseMoveEvent(self, event):
+        if self.scaledPixmap.rect().contains(event.pos()):
+            x = event.pos().x() * self.sx
+            y = event.pos().y() * self.sy
+            self.currentTerritory = self.territoryAt(x, y)
+        else:
+            self.currentTerritory = None
+        if self.currentTerritory:
+            self.setToolTip(self.currentTerritory.name)
+        else:
+            self.setToolTip("")
+        self.update()
+
     def mouseReleaseEvent(self, event):
-        pos = [event.pos().x(), event.pos().y()]
-        pos[0] *= self.sx
-        pos[1] *= self.sy
-        t = self.territoryAt(*pos)
-        if t:
-                print "Clicked on %s." % (t.name)
+        if self.currentTerritory:
+            print "Clicked on %s." % (self.currentTerritory.name)
 
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.drawPixmap(0, 0, self.scaledPixmap)
+        if self.currentTerritory:
+            painter.drawImage(0, 0, self.selectionImages[self.currentTerritory])
         painter.end()
--- common/board.py
+++ common/board.py
@@ -10,6 +10,9 @@ class Territory(object):
     def __eq__(self, other):
         return self.name == other.name
 
+    def __hash__(self):
+        return hash(self.name)
+
     def __str__(self):
         return self.name
 
