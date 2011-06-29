40e6dfa8f82c1a93c4b2cccecc63a7ecea7707b9
Diony Rosa <dhrosa@gmail.com>
2011-06-29 12:42:06 -0400

    Made PlayerInfo read-only, and made it stretch to fill space.

--- client/playerinfo.py
+++ client/playerinfo.py
@@ -1,5 +1,5 @@
 from PyQt4.QtCore import pyqtSignal, Qt
-from PyQt4.QtGui import QTableWidget, QTableWidgetItem, QColor
+from PyQt4.QtGui import QTableWidget, QTableWidgetItem, QHeaderView, QColor
 
 class PlayerInfo(QTableWidget):
     (Name, Color, Cards, TroopsPerTurn) = range(4)
@@ -9,18 +9,30 @@ class PlayerInfo(QTableWidget):
         columns = ["Name", "Color", "Cards", "Troops Per Turn"]
         self.setColumnCount(len(columns))
         self.setHorizontalHeaderLabels(columns)
+        self.horizontalHeader().setResizeMode(QHeaderView.ResizeToContents)
+        self.horizontalHeader().setResizeMode(QHeaderView.Stretch)
 
     def addPlayer(self, name, color):
         r = self.rowCount()
         self.setRowCount(r + 1)
         print name, color, r
         name = QTableWidgetItem(name)
+        name.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
         colorItem = QTableWidgetItem()
+        colorItem.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
         colorItem.setBackground(QColor(*color))
+
         cards = QTableWidgetItem()
+        cards.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+        cards.setTextAlignment(Qt.AlignCenter)
         cards.setData(Qt.DisplayRole, 0)
+
         troops = QTableWidgetItem()
+        troops.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+        troops.setTextAlignment(Qt.AlignCenter)
         troops.setData(Qt.DisplayRole, 0)
+
         self.setItem(r, self.Name, name)
         self.setItem(r, self.Color, colorItem)
         self.setItem(r, self.Cards, cards)
